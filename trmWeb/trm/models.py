49bd48e927daa7cadec68b97e607014af86f8250
Benedito Momade Braimo <beneditobraimo@gmail.com>
2024-02-02 12:11:48 +0200

    Created location model

--- trmWeb/trm/models.py
+++ trmWeb/trm/models.py
@@ -16,3 +16,10 @@ class Recommendation(models.Model):
 
     def __str__(self):
         return (self.description, self.relevance)
+    
+class Location(models.Model):
+    name = models.CharField(max_length=60, blank=False)
+    description = models.Charfield(max_length=100)
+
+    def __str__(self):
+        return (self.name)

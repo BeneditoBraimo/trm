afefbdc4718ae2144f17afb797c68ce649f12fda
Benedito Momade Braimo <beneditobraimo@gmail.com>
2024-02-02 11:40:31 +0200

    Created Agent class

--- trmWeb/trm/models.py
+++ trmWeb/trm/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Agent(models.Model):
+    firstName = models.CharField(max_length=50, blank=False)
+    lastName = models.CharField(max_length=50, blank=False)
+    code = models.CharField(max_length=11, blank=False, unique=True)
+
+    def __str__(self):
+        return str(self.firstName, self.lastName)

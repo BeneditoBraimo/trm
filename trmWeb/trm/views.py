90351adf522c7b8f3820eb96cbcc7e499f295328
Benedito Momade Braimo <beneditobraimo@gmail.com>
2024-02-08 15:51:38 +0200

    added code to display event form if it's not validated

--- trmWeb/trm/views.py
+++ trmWeb/trm/views.py
@@ -6,9 +6,12 @@ def index(request):
     return render(request, "index.html", {'title':title})
 
 def addEvent(request):
-    title = 'Event added successfully'
+    title = 'Event form'
     if request.method == 'POST':
         event = EventForm(request.POST)
         if event.is_valid():
             event.save()
-    return render(request,'index.html', {'title': title})
+            return render(request,'index.html', {'title': title})
+        else:
+            event = EventForm()
+    return render(request, "addEventForm.html", {'title': title})

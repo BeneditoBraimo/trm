2c61b6196a10f91d33d2824a95658f116ec9b3c5
Benedito Momade Braimo <beneditobraimo@gmail.com>
2024-02-08 14:05:27 +0200

    imported all classes from forms.py to the views.py module

--- trmWeb/trm/views.py
+++ trmWeb/trm/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import AgentForm, RecommendationForm, OccurrenceForm, EventForm, LocationForm
 
 def index(request):
     title = 'Home'

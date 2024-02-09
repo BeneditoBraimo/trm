1cee8782d63f2ae54e29fd8d2ba58ff58cd7debf
Benedito Momade Braimo <beneditobraimo@gmail.com>
2024-02-09 15:52:24 +0200

    deleted code that used a removed module

--- trmWeb/trm/views.py
+++ trmWeb/trm/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import AgentForm, RecommendationForm, OccurrenceForm, EventForm, LocationForm
+
 
 def index(request):
     title = 'Home'
@@ -7,11 +7,5 @@ def index(request):
 
 def addEvent(request):
     title = 'Event form'
-    event = EventForm()
-    if request.method == 'POST':
-        event = EventForm(request.POST)
-        if event.is_valid():
-            event.save()
-            return render(request,'index.html', {'title': title})
-            
-    return render(request, "addEventForm.html", {'form': event})
+    
+    return render(request, "addEventForm.html", {'title': title})

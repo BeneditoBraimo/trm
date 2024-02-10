80dd226af54b2bb4d59f9a4fc27404aa47d50805
Benedito Momade Braimo <beneditobraimo@gmail.com>
2024-02-10 16:41:40 +0200

    added code to get form data from events form

--- trmWeb/trm/views.py
+++ trmWeb/trm/views.py
@@ -7,5 +7,10 @@ def index(request):
 
 def addEvent(request):
     title = 'Event form'
-    
+    if request.method == 'POST':
+        startingDate = request.POST.get('startingDate')
+        eventTitle = request.POST.get('eventTitle')
+        eventDescription = request.POST.get('eventDescription')
+
+        eventLocation = request.POST.get('eventLocation')
     return render(request, "addEventForm.html", {'title': title})

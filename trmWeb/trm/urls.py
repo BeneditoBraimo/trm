a5d43ed81637e82d61d4db378d41379a78f54df0
Benedito Momade Braimo <beneditobraimo@gmail.com>
2024-02-11 13:00:22 +0200

    added a path to 'addLocation' view

--- trmWeb/trm/urls.py
+++ trmWeb/trm/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("addEvent/", views.addEvent, name="addEvent")
+    path("addEvent/", views.addEvent, name="addEvent"),
+    path("addLocation/", views.addLocation, name="addLocation"),
 ]
